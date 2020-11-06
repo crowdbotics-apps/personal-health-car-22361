6006b8d083fd99bb0f43958d53e84912965ad78a
Crowdbotics <team@crowdbotics.com>
2020-11-06 21:10:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/personal_health_car_22361/MainActivity.java
@@ -0,0 +1,15 @@
+package com.personal_health_car_22361;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "personal_health_car_22361";
+    }
+}
